09f11dc8d3f2bff02fc277d5f4c877f6c88763f8
Matthew Chen <matthewchen8462@hotmail.com>
2019-09-16 00:37:29 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,16 @@
-var cardOne = "queen";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-var cardTwo = "queen";
-
-var cardThree = "king";
-
-var cardFour = "king";
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.")
+}
